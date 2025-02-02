1518f13bf51a1f836c763ee35cf51c75e5549095
ricoerian <richoeriansyahm@gmail.com>
2025-02-03 01:38:38 +0700

    edit callback

--- src/middleware/OauthCallback.tsx
+++ src/middleware/OauthCallback.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 
 const OauthCallback = () =>{
-    return <Link to={'https://auth-golang-production-8253.up.railway.app/'}></Link>
+    return <Link to={'https://auth-golang-production-8253.up.railway.app/oauth/google/callback'}></Link>
 }
 
 export default OauthCallback
