ab147bfc16bb7a4fbe7d7cfd995e635d1e2885cd
ricoerian <richoeriansyahm@gmail.com>
2025-02-03 01:42:23 +0700

    edit oauth callback

--- src/middleware/OauthCallback.tsx
+++ src/middleware/OauthCallback.tsx
@@ -1,7 +1,9 @@
-import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 const OauthCallback = () =>{
-    return <Link to={'https://auth-golang-production-8253.up.railway.app/oauth/google/callback'}></Link>
+    const navigate = useNavigate()
+    navigate('https://auth-golang-production-8253.up.railway.app/oauth/google/callback')
+    return <p>OatuhCallback</p>
 }
 
 export default OauthCallback
