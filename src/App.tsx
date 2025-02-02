3e89295cf5d9c555e458bcb4ce0031622ee84169
ricoerian <richoeriansyahm@gmail.com>
2025-02-03 01:35:34 +0700

    create callback

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ProtectedRoute from "./middleware/ProtectedRoute";
 import Dashboard from "./pages/Dashboard";
+import OauthCallback from "./middleware/OauthCallback";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/oauth/google/callback" element={<OauthCallback />} />
         <Route
           path="/dashboard"
           element={
---
+++ src/middleware/OauthCallback.tsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom"
+
+const OauthCallback = () =>{
+    return <Link to={'https://auth-golang-production-8253.up.railway.app/'}></Link>
+}
+
+export default OauthCallback
